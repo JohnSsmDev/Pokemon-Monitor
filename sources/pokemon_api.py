c2ee3d851f51b3fc0e9ea7c1b222e2bf11c3e0ab
JohnSsmDev <joaosousamelodev@gmail.com>
2026-05-10 17:46:33 -0300

    improve find ranking

--- sources/pokemon_api.py
+++ sources/pokemon_api.py
@@ -42,18 +42,41 @@ def search_cards(query):
                 or prices.get("trendPrice")
                 or 0
             )
+            match_score = 0
 
-            cards.append({
+            if c.get("number") == query:
+                match_score += 100
+
+            rarity = str(c.get("rarity", "")).lower()
+
+            rare_words = [
+                "secret",
+                "ultra",
+                "illustration",
+                "hyper",
+                "full"
+            ]
+
+            for w in rare_words:
+                if w in rarity:
+                    match_score += 30
+
+                cards.append({
                 "name": c.get("name"),
                 "number": c.get("number"),
                 "rarity": c.get("rarity"),
                 "set": c.get("set", {}).get("name"),
                 "image": c.get("images", {}).get("large"),
                 "price": round(float(price), 2),
-                "source": "PokemonTCG"
-            })
+                "source": "PokemonTCG",
+                "match_score": match_score
+})
 
         except:
             continue
-
+            cards = sorted(
+    cards,
+    key=lambda x: x["match_score"],
+    reverse=True
+)
     return cards
